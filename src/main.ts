082f1334181a844deb0fb5547c2d67c24e720b05
Belen Baez <bnoeliabelen@gmail.com>
2025-08-02 18:17:07 -0300

    refactor: implementar interceptor de autenticación para gestionar tokens en las solicitudes HTTP

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http'; // ✅ Importar esto
-import 'zone.js'; // ✅ necesario para Angular
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import 'zone.js';
+import { authInterceptor } from './app/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // ✅ Esto habilita HttpClient en la app
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+    )
   ]
 }).catch(err => console.error(err));
+
+
+
+
