9216ac5d7797005c49f15b639b1f6d327d48464d
nilsonavalos77 <nilson.avalos94@gmail.com>
2025-08-03 21:23:44 -0300

    refactor: eliminar componente de inicio y ajustar rutas, mejorando la estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './componentes/home.component';
 import { EventosComponent } from './componentes/eventos.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegisterComponent } from './componentes/register/register.component';
@@ -9,8 +8,7 @@ import { InformacionComponent } from './componentes/informacion/informacion.comp
 import { ContactosComponent } from './componentes/contactos/contactos.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'eventos', pathMatch: 'full' },
   { path: 'eventos', component: EventosComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
